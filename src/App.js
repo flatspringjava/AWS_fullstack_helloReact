9cb86cc57e284b75d45890d62dc7dd8a53107502
flatjava <wodnjsdl01@gmail.com>
2023-05-08 09:49:27 +0900

    commit message;

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import MyComponent from './MyComponent';
+import Say from "./say";
 
 const App = () => {
-  return <MyComponent name="아자차카타하">React</MyComponent>;
+  return <Say />;
 };
 
 export default App;
 
-
-
 //d asdasdasdawedaqwdasd
 
+// '', false, 0, null, undefined
 
+// var, let 키워드도 사용 가능.
+// const name = "리액트2";
+// let number = 0;
+// let text = "";
+// if(name == "JSX") {
+//   text = '<h1>리액트 입니다.</h1>';
+// }
+// else {
+//   text = '<h2>리액트가 아닙니다.</h2>'
+// }
 
-
-
-
-
-  // '', false, 0, null, undefined
-
-  // var, let 키워드도 사용 가능.
-  // const name = "리액트2";
-  // let number = 0;  
-  // let text = "";
-  // if(name == "JSX") {
-  //   text = '<h1>리액트 입니다.</h1>';
-  // }
-  // else {
-  //   text = '<h2>리액트가 아닙니다.</h2>'
-  // }
-  
-  // return (
-  //   <Fragment>
-  //     {
-  //       number && <h1>리액트 입니다.</h1>
-  //       /* { {name === "JS" ? (<h1>리액트 입니다.</h1>) : (<h2>리액트가 아닙니다.</h2>)} }*/
-  //     }
-  //   </Fragment>
-  // );
+// return (
+//   <Fragment>
+//     {
+//       number && <h1>리액트 입니다.</h1>
+//       /* { {name === "JS" ? (<h1>리액트 입니다.</h1>) : (<h2>리액트가 아닙니다.</h2>)} }*/
+//     }
+//   </Fragment>
+// );
