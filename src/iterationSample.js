2ebda13b4436dcf03533406e8dd83eb462b4d809
flatjava <wodnjsdl01@gmail.com>
2023-05-09 10:58:39 +0900

    commit message gd

--- src/iterationSample.js
+++ src/iterationSample.js
@@ -13,12 +13,19 @@ const IterationSample = () => {
   const [names, setNames] = useState(obj);
   const [inputText, setInputText] = useState("");
   const [nextId, setNextId] = useState(5);
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
 
+  const onRemove = (id) => {
+    setNames(names.filter((name) => name.id !== id));
+  };
   const onChange = (e) => setInputText(e.target.value);
   const onClick = () => {
     const nextName = names.concat({ id: nextId, text: inputText });
-    setNextId(nextId);
+    setNextId(nextId + 1);
     setNames(nextName);
     setInputText("");
   };
