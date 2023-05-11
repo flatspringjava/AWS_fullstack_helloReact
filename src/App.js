9702b4c59e0d4d6403efab7e1dff69662e93e998
flatjava <wodnjsdl01@gmail.com>
2023-05-11 13:50:45 +0900

    news-viewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "../node_modules/axios/index";
+import { useState } from "react";
 
+// const onClick = () => {
+//   const [data, setData] = useState();
+//   Axios.get("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
+//     setData(response.data);
+//   });
+// };
 function App() {
+  const [data, setData] = useState();
+  const onClick = async () => {
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/todos/1")
+    //   .then((response) => {
+    //     setData(response.data);
+    //   });
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    setData(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
 }
